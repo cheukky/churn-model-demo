10e4ea8c48743b4a84498dd6cb7a56d56a1b6bed
Cheuk Tse <cheuk.tse@infiniaml.com>
2018-10-24 11:13:00 -0400

    stuff

--- common/data.py
+++ common/data.py
@@ -1,11 +1,11 @@
-## File of helper functions for handling data. Copied from various common modules. 
+## File of helper functions for handling data. Copied from various common modules.
 
 import os
 import pandas as pd
 import pickle
 import logging
 from skafossdk import DataSourceType, Skafos
-from s3fs.core import S3FileSystem  
+from s3fs.core import S3FileSystem
 from .schema import SCORING_SCHEMA
 
 
@@ -17,26 +17,26 @@ TRAINING_FILE_NAME = "TelcoChurnData/WA_Fn-UseC_-Telco-Customer-Churn_train.csv"
 SCORING_FILE_NAME = "TelcoChurnData/WA_Fn-UseC_-Telco-Customer-Churn_score.csv"
 
 # Output models and scores
-S3_PRIVATE_BUCKET = "skafos.example.output.data"
+S3_PRIVATE_BUCKET = "skafos.tutorial"
 CHURN_MODEL_SCORES = "TelcoChurnData/churn_model_scores/scores.csv"
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 # Project Token
-KEYSPACE = "91633e3d419e23dc7a2da419"
+KEYSPACE = "8a662ae31c8e62fc4da80e5f"
 
 
 #-------------------Data Access Functions -----------------------
 
-# Get input data from S3 -- specify training or scoring. 
-def get_data(csvCols, whichData):  
+# Get input data from S3 -- specify training or scoring.
+def get_data(csvCols, whichData):
     s3 = S3FileSystem(anon=True)
     if whichData == "training":
         path = f's3://{S3_BUCKET}/{TRAINING_FILE_NAME}'
     elif whichData == "scoring":
         path = f's3://{S3_BUCKET}/{SCORING_FILE_NAME}'
     # PUT ERROR CATCHING HERE FOR ERRORS IN INPUT FILES
-    # Read in .csv file, but only for specified columns. 
+    # Read in .csv file, but only for specified columns.
     df = pd.read_csv(s3.open(f'{path}', mode='rb'), usecols=csvCols)
     for c in csvCols:
         if (df[c].dtype == 'object'):
@@ -51,7 +51,7 @@ def save_scores(ska, scoring, location):
         #Save to Cassandra
         ska.log("Saving to Cassandra", level=logging.INFO)
         ska.engine.save(SCORING_SCHEMA, scores).result()
-        ska.log("Saving to Cassandra", labels=["S3saving"], level=logging.INFO)   
+        ska.log("Saving to Cassandra", labels=["S3saving"], level=logging.INFO)
     #Save to S3
     if location=="both" or location=="S3":
         bytes_to_write = scoring.to_csv(None, index=False).encode()
@@ -59,8 +59,8 @@ def save_scores(ska, scoring, location):
         with fs.open(f"s3://{S3_PRIVATE_BUCKET}/{CHURN_MODEL_SCORES}", 'wb') as f:
                 f.write(bytes_to_write)
         ska.log("Saving to S3", labels=["S3saving"], level=logging.INFO)
-    
-            
+
+
 def save_model(ska, model_id, fittedModel, modelType):
     ska.log("Saving model to S3", labels=["S3saving"], level=logging.INFO)
     fileName = f"model_id_{model_id}_{modelType}.pkl"
@@ -78,9 +78,9 @@ def get_model(ska, model_id, modelType):
     fs = S3FileSystem(key=AWS_ACCESS_KEY_ID, secret=AWS_SECRET_ACCESS_KEY)
     with fs.open(filePath, mode="rb") as f:
         return pickle.loads(f.read())
-    
 
-#-------------------Data Manipulation Functions ----------------------- 
+
+#-------------------Data Manipulation Functions -----------------------
 
 # Convert categorical variables to dummies
 def dummify_columns(xVars, features):
@@ -98,6 +98,5 @@ def dummify_columns(xVars, features):
                 newDF = xVars['total_charges'].apply(pd.to_numeric)
                 xVars = xVars.drop('total_charges', axis=1)
                 xVars['total_charges'] = newDF
-         
-    return xVars     
 
+    return xVars
